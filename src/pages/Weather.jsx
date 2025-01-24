c2f8307e531066a29e50b609730449e607d24431
Emerson Frasure <emersonfrasure@gmail.com>
2025-01-24 12:21:35 -0500

    Card styling
    Styling for weather, card, and adds a second modal to weather, non-functional.

--- src/pages/Weather.jsx
+++ src/pages/Weather.jsx
@@ -4,8 +4,18 @@ import Card from '../components/Card'
 import '../styles/weather.css'
 
 function Weather() {
-    const [onOffTime, setOnOffTime] = useState({startTime: "00:00", endTime: "23:59"})
-    const [modalOpen, setModalOpen] = useState(false)
+    /**
+     * display data will look something like
+     * 
+     * const displayData = {
+     *   startTime: "00:00",
+     *   endTime: "23:59",
+     *   text: "Hello World"
+     * }
+     */
+    const [displayData, setDisplayData] = useState({})
+    const [timeModalOpen, setTimeModalOpen] = useState(false)
+    const [textModalOpen, setTextModalOpen] = useState(false)
 
     useEffect(() => {
         //Make a request to the server to get the current time set to display
@@ -13,30 +23,54 @@ function Weather() {
 
     function changeTime() {
         //pop up a change time modal maybe
-        //use setOnOffTime to change the time of state then send that to the server
-        setModalOpen(true);
+        //use setDisplayData to change the time of state then send that to the server
+        setTimeModalOpen(true);
   }
 
     return (
         <div className="weather">
+            <h1>Weather Display</h1>
             <Card
-                titleContent={<h1>Weather</h1>}
+                titleContent={<h1>On/Off Time</h1>}
                 content={
-                    <p>Current time set to display: {onOffTime.startTime} to {onOffTime.endTime}</p>
+                    <p>Current time set to display: {displayData.startTime} to {displayData.endTime}</p>
                 }
                 footerContent={
                     <button onClick={changeTime}>Change Time</button>
                 }
             />
+
+            <Card 
+                titleContent={<h1>Text Display</h1>}
+                content={<p>Displaying: {displayData.text}</p>}
+                footerContent={
+                    <button onClick={() => setTextModalOpen(true)}>Change Text</button>
+                }
+            />
+
             <Modal 
-                open={modalOpen}
-                titleContent={<h1> Close </h1>}
-                cancelFn={() => setModalOpen(false)}
-                primaryFn={() => setModalOpen(false)}
-                secondaryFn={() => setModalOpen(false)}
+                open={timeModalOpen}
+                titleContent={<h1> Change Time </h1>}
+                cancelFn={() => setTimeModalOpen(false)}
+                primaryFn={() => setTimeModalOpen(false)}
+                secondaryFn={() => setTimeModalOpen(false)}
                 content={
                    <>
-                     <h2>This is a modal</h2>
+                     <h2>This is a time modal</h2>
+                     <p>You can close it by pressing Escape key, pressing close, or clicking outside the modal.</p>
+                  </>
+
+               }
+           />
+            <Modal 
+                open={textModalOpen}
+                titleContent={<h1> Change Text </h1>}
+                cancelFn={() => setTextModalOpen(false)}
+                primaryFn={() => setTextModalOpen(false)}
+                secondaryFn={() => setTextModalOpen(false)}
+                content={
+                   <>
+                     <h2>This is a text modal</h2>
                      <p>You can close it by pressing Escape key, pressing close, or clicking outside the modal.</p>
                   </>
 
