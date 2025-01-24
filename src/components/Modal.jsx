4b726a006e61a1c7ee1563f481fc6ac1180aae37
Emerson Frasure <emersonfrasure@gmail.com>
2025-01-24 11:49:12 -0500

    Modal component w/ styling, not functionally added

---
+++ src/components/Modal.jsx
@@ -0,0 +1,52 @@
+import { useEffect } from 'react'
+import '../styles/modal.css'
+
+function Modal(props) {
+    const {open, cancelFn, primaryFn, secondaryFn, closeIcon, titleContent, content} = props;
+
+    // capture ESC key to close the modal 
+    useEffect(() => {
+        const handleKeyDown = (e) => {
+        if (e.key === 'Escape' && open) {
+            if (cancelFn) {
+                cancelFn()
+            }
+        }
+    }
+        document.addEventListener('keydown', handleKeyDown);
+        return () => document.removeEventListener('keydown', handleKeyDown);
+    }, [open, cancelFn])
+
+    if (!open) return null;
+
+    return (
+<div className="modalBackground">
+            <div className="modalContainer">
+                {titleContent && (<div className="title">
+                        {titleContent}
+                        <div className="titleCloseBtn">
+                            <button onClick={cancelFn}>{closeIcon ?? 'X'}</button>
+                        </div>
+                    </div>
+                )}
+
+                <div className="body">
+                    {content}
+                </div>
+
+                <div className="footer">
+                    {secondaryFn && (
+                        <button onClick={secondaryFn} id="cancelBtn">
+                            Cancel
+                        </button>
+                    )}
+                    {primaryFn && (
+                        <button onClick={primaryFn}>Continue</button>
+                    )}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Modal
--- src/pages/Weather.jsx
+++ src/pages/Weather.jsx
@@ -1,25 +1,42 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
+import Modal from '../components/Modal'
 
 function Weather() {
-  const [onOffTime, setOnOffTime] = useState({startTime: "00:00", endTime: "23:59"})
+    const [onOffTime, setOnOffTime] = useState({startTime: "00:00", endTime: "23:59"})
+    const [modalOpen, setModalOpen] = useState(false)
 
-  useEffect(() => {
-    //Make a request to the server to get the current time set to display
-  }, [])
+    useEffect(() => {
+        //Make a request to the server to get the current time set to display
+    }, [])
 
-  function changeTime() {
-    //pop up a change time modal maybe
-    //use setOnOffTime to change the time of state then send that to the server
+    function changeTime() {
+        //pop up a change time modal maybe
+        //use setOnOffTime to change the time of state then send that to the server
+        setModalOpen(true);
   }
 
-  return (
-    <div className="weather">  
-      <h1>Weather</h1>
-      <p>Current time set to display: {onOffTime.startTime} to {onOffTime.endTime}</p>
-      <button onClick={changeTime}>Change Time</button>
-    </div>
-  )
+    return (
+        <div className="weather">  
+            <h1>Weather</h1>
+            <p>Current time set to display: {onOffTime.startTime} to {onOffTime.endTime}</p>
+            <button onClick={changeTime}>Change Time</button>
+            <Modal 
+                open={modalOpen}
+                titleContent={<h1> Close </h1>}
+                cancelFn={() => setModalOpen(false)}
+                primaryFn={() => setModalOpen(false)}
+                secondaryFn={() => setModalOpen(false)}
+                content={
+                   <>
+                     <h2>This is a modal</h2>
+                     <p>You can close it by pressing Escape key, pressing close, or clicking outside the modal.</p>
+                  </>
+
+               }
+           />
+        </div>
+    )
 }
 
 export default Weather
