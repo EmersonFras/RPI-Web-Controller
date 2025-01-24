156bd71ad4d89cda46dbd62284598d16f15d825f
Emerson Frasure <emersonfrasure@gmail.com>
2025-01-24 11:21:18 -0500

    Basic filler text and comments for improvements

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className="home">    
+      <h1>Home</h1>
+      <p>Welcome to RPI control display</p>
+    </div>
   )
 }
 
--- src/pages/Weather.jsx
+++ src/pages/Weather.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 
 function Weather() {
+  const [onOffTime, setOnOffTime] = useState({startTime: "00:00", endTime: "23:59"})
+
+  useEffect(() => {
+    //Make a request to the server to get the current time set to display
+  }, [])
+
+  function changeTime() {
+    //pop up a change time modal maybe
+    //use setOnOffTime to change the time of state then send that to the server
+  }
+
   return (
-    <div>Weather</div>
+    <div className="weather">  
+      <h1>Weather</h1>
+      <p>Current time set to display: {onOffTime.startTime} to {onOffTime.endTime}</p>
+      <button onClick={changeTime}>Change Time</button>
+    </div>
   )
 }
 
