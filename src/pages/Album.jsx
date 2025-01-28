3a82d1acd94b853fe24fc128f9656db9c49f5be4
emersonfras <emersonfrasure@gmail.com>
2025-01-27 20:03:51 -0500

    Conditional Album Rendering

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -18,6 +18,17 @@ function Album() {
         }).then((res) => {setAlbumData(res.data.albums.items)})
     }
 
+    const albumElement = (
+        <div className='album-container'>
+            <div key={albumData[currentAlbum].id} className='album-item'>
+                <img src={albumData[currentAlbum].image.url} alt={albumData[currentAlbum].name} />
+                <p>{albumData[currentAlbum].name}</p>
+            </div>
+            <button onClick={setCurrentAlbum((prev) => {prev == 9 ? 0 : prev++})}>Next</button>
+            <button onClick={setCurrentAlbum((prev) => (prev == 0 ? 9 : prev--))}>Previous</button>
+        </div>
+    )
+
     console.log(albumData)
 
     return (
@@ -51,12 +62,16 @@ function Album() {
                 secondaryFn={() => setAlbumModalOpen(false)}
                 content={
                     <div className='album-container'>
-                        <div key={albumData[currentAlbum].id} className='album-item'>
-                            <img src={albumData[currentAlbum].image.url} alt={albumData[currentAlbum].name} />
-                            <p>{albumData[currentAlbum].name}</p>
-                        </div>
-                        <button onClick={setCurrentAlbum((prev) => {prev == 9 ? 0 : prev++})}>Next</button>
-                        <button onClick={setCurrentAlbum((prev) => (prev == 0 ? 9 : prev--))}>Previous</button>
+                        {albumData.length > 0 ? 
+                        <>
+                            <div key={albumData[currentAlbum].id} className='album-item'> 
+                                <img src={albumData[currentAlbum].image.url} alt={albumData[currentAlbum].name} />
+                                <p>{albumData[currentAlbum].name}</p>
+                            </div>
+                            <button onClick={setCurrentAlbum((prev) => {prev == 9 ? 0 : prev++})}>Next</button>
+                            <button onClick={setCurrentAlbum((prev) => (prev == 0 ? 9 : prev--))}>Previous</button>
+                        </>
+                        : undefined}
                     </div>
                }
            />
